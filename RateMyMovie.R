0dcc4fcdc7c3bd2037db50a6483695fbd65d4f30
George Mathew <george.meg91@gmail.com>
2015-03-30 20:03:02 -0400

    Writing into CSV from rotten tomatoes

--- RateMyMovie.R
+++ RateMyMovie.R
@@ -22,12 +22,30 @@ getReviews<-function(movieName="Dark Knight Rises", apiKey="qynq4687htc3z7mq2ec7
   rating = getRating(movieObj$movies$ratings$audience_score)
   id = movieObj$movies$id
   reviewsURL=paste("http://api.rottentomatoes.com/api/public/v1.0/movies/",
-                   id, "/reviews.json?review_type=all&page_limit=50&page=1&country=us&apikey=", 
+                   id, "/reviews.json?review_type=all&page_limit=5&page=1&country=us&apikey=", 
                    apiKey, sep="")
   jsonStr=getURLContent(reviewsURL)
   reviews=fromJSON(jsonStr[1])$reviews$quote
   movieReview=data.frame(name=movieName, id=id, rating=rating, reviews=reviews)
   return(movieReview)
+}
+
+makeTrainSet<-function(moviesFileName="movies.txt", output="train.csv") {
+  if (file.exists(output)) {
+    file.remove(output)
+  }
+  moviesFile=file(moviesFileName, open = "r")
+  movies = readLines(moviesFile)
+  close(moviesFile)
+  doHeader = TRUE
+  for (i in 1:length(movies)) {
+    reviews = getReviews(movieName=movies[i])
+    write.table(reviews, file=output, sep=",", col.names=doHeader, row.names=FALSE, append = !doHeader)
+    if (doHeader) {
+      doHeader = FALSE
+    }
+    Sys.sleep(0.5)
+  }
 }
 
 
